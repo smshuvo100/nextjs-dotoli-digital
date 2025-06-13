486588e00a29cdca7b36f40c3a4d935de32801b0
smshuvo100 <smshuvo100@gmail.com>
2025-06-13 23:44:10 +0600

    14

--- src/app/services/ai-agents-chatbots/page.jsx
+++ src/app/services/ai-agents-chatbots/page.jsx
@@ -12,17 +12,7 @@ export default function Page() {
         title2="Intelligent Customer Engagement."
         description={`Unlock the power of artificial intelligence in customer service and sales.<br/> We create bespoke AI agents and chatbots that converse naturally,<br/> automate support, and boost conversions - all while embodying your brand's voice.`}
       />
-      <PortfolioImage
-        img1="/images/project-1.webp"
-        img2="/images/project-2.webp"
-        img3="/images/project-3.webp"
-        img4="/images/project-4.webp"
-        img5="/images/project-5.webp"
-        img6="/images/project-6.webp"
-        img7="/images/project-7.webp"
-        img8="/images/project-8.webp"
-        img9="/images/project-4.webp"
-      />
+      <PortfolioImage img1="/images/ai1.webp" img2="/images/ai2.webp" img3="/images/ai3.webp" img4="/images/ai4.webp" />
     </>
   );
 }
