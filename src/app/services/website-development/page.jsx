7a0e4d49b95cf520d1c0612a7b7f6521c0dcef54
smshuvo100 <smshuvo100@gmail.com>
2025-06-17 00:40:20 +0600

    18

--- src/app/services/website-development/page.jsx
+++ src/app/services/website-development/page.jsx
@@ -13,8 +13,8 @@ export default function Page() {
         description={`Our web development team crafts high-end, mobile-responsive websites<br/> that not only look exceptional but also drive business growth.Specializing<br/>   in sitesthat wow investors and customers alike.`}
       />
       <PortfolioImage
-        img1="/images/w1.webp"
-        img2="/images/w2.webp"
+        img1="/images/w2.webp"
+        img2="/images/w1.webp"
         img3="/images/w3.webp"
         img4="/images/w4.webp"
         img5="/images/w5.webp"
