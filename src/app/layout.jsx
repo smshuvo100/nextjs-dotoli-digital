28852c68e252ee2a704c0bff3cd86d819534e5bf
smshuvo100 <smshuvo100@gmail.com>
2025-06-26 18:50:40 +0600

    37

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -36,8 +36,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${abrilFatface.variable} ${inter.variable}`}>
+      <head>
+        {/* Google Tag Manager - Head */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5Z686QQK');
+            `
+          }}
+        />
+      </head>
       {/* inter */}
       <body>
+        {/* Google Tag Manager - NoScript (must be first element in <body>) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z686QQK" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <LoadingWrapper>
             <Header />
